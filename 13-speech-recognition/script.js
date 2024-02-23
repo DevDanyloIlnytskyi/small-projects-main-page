a7990b6ebf82456f55ae83d79189513d7915ac2e
Danylo <dev.danylo.ilnytskyi@gmail.com>
2024-02-23 20:52:41 +0200

    add gimn

--- 13-speech-recognition/script.js
+++ 13-speech-recognition/script.js
@@ -1,10 +1,16 @@
+const audio = document.getElementById('audio');
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
 const rec = new SpeechRecognition();
 
+
 // rec.lang = 'en-US';
 rec.lang = 'uk-UA';
 rec.continuous = true;
+
+const audio2 = new Audio('./music/gimn_ukrani.mp3');
+const div = document.querySelector('.audio');
+div.appendChild(audio2);
 
 rec.onresult = function (e) {
     // const acceptedColors = [
@@ -32,6 +38,7 @@ rec.onresult = function (e) {
         ['white', 'білий'],
         ['white', 'слава україні'],
     ];
+
     
     for (let i = e.resultIndex; i < e.results.length; i++) {
         const script = e.results[i][0].transcript.toLowerCase().trim();
@@ -41,6 +48,7 @@ rec.onresult = function (e) {
             if (script === 'слава україні') {
                 document.body.style.background = 'linear-gradient(to top, #eeff00, #eeff00 50%, #1100ff 50%)';
                 document.querySelector('h1').innerText = 'Героям Слава!';
+                audio2.play();
             } else {
                 document.body.style.background = '';
                 document.querySelector('h1').innerText = 'Назвіть колір';
